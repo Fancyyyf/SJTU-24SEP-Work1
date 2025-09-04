366b3b2dae8cf1ef1a249d38e83d8be0fca5883c
FrancisInnocence <fancy.candy@sjtu.edu.cn>
2025-09-04 17:08:13 +0800

    Add files via upload

---
+++ lab3-100/cmake-build-debug/CMakeFiles/lab3_test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_test.dir/lab3_test.cpp.obj"
+  "CMakeFiles/lab3_test.dir/lab3_test.cpp.obj.d"
+  "lab3_test.exe"
+  "lab3_test.exe.manifest"
+  "lab3_test.pdb"
+  "lab3_test[1]_tests.cmake"
+  "liblab3_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
