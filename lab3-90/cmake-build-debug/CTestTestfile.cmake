ea242b4c2b61402a7aa55b275c10275c7c4ce2db
FrancisInnocence <fancy.candy@sjtu.edu.cn>
2025-09-04 17:03:11 +0800

    Add files via upload

---
+++ lab3-90/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Virtual Desktop/lab3/lab3
+# Build directory: E:/Virtual Desktop/lab3/lab3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("E:/Virtual Desktop/lab3/lab3/cmake-build-debug/lab3_test[1]_include.cmake")
+subdirs("googletest")
